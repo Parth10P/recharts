Parth10P/recharts

commit 19c7abcebcd5d84c50defdb6eca0492e543e9bc6

Rendered ticks in hooks (#6990)
## Description

The problem is that CartesianAxis uses DOM to calculate the actual font
size and overlap, and then uses that to decide which ticks get rendered.
That's legit but doesn't fit in the reducer nor selector. So I added a
new slice where we effectively cache the calculation and return it from
the tick hooks.

I have also removed the incorrect domain as the robot told me in the
other PR.

## Related Issue

Fixes https://github.com/recharts/recharts/issues/6978

## Screenshots (if appropriate):


https://github.com/user-attachments/assets/10e4b856-97ad-4bd6-83f6-f898fd67a4f1

## Types of changes

- [x] Bug fix (non-breaking change which fixes an issue)
- [ ] New feature (non-breaking change which adds functionality)
- [ ] Breaking change (fix or feature that would cause existing
functionality to change)

## Checklist:

- [ ] My change requires a change to the documentation.
- [ ] I have updated the documentation accordingly.
- [ ] I have added tests to cover my changes.
- [x] I have added a storybook story or VR test, or extended an existing
story or VR test to show my changes


<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->
## Summary by CodeRabbit

* **New Features**
* Added fontSize and letterSpacing props to XAxis and YAxis for finer
tick text control.
* Axes can now be identified with an axisId and publish rendered tick
metadata to shared state for improved multi-axis coordination.
* **Documentation**
  * Examples updated to use automatic Y axis width in relevant demos.
* **Tests**
* Test expectations adjusted to reflect the new rendered-ticks behavior
and additional render passes.
<!-- end of auto-generated comment: release notes by coderabbit.ai -->

src/cartesian/CartesianAxis.tsx
@@ -2,14 +2,14 @@
  * @fileOverview Cartesian Axis
  */
 import * as React from 'react';
-import { SVGProps, useState, useRef, useCallback, forwardRef, useImperativeHandle } from 'react';
+import { SVGProps, useState, useRef, useCallback, forwardRef, useImperativeHandle, useEffect } from 'react';
 
 import get from 'es-toolkit/compat/get';
 import { clsx } from 'clsx';
 import { Layer } from '../container/Layer';
 import { Text, Props as TextProps, TextAnchor, TextVerticalAnchor } from '../component/Text';
 import { CartesianLabelContextProvider, ImplicitLabelType, CartesianLabelFromLabelProp } from '../component/Label';
-import { isNumber } from '../util/DataUtils';
+import { isNumber, noop } from '../util/DataUtils';
 import {
   CartesianViewBox,
   adaptEventsOfChild,
@@ -22,15 +22,18 @@ import {
   BaseTickContentProps,
   XAxisTickContentProps,
   YAxisTickContentProps,
+  TickItem as TickItemType,
 } from '../util/types';
 import { getTicks } from './getTicks';
 import { svgPropertiesNoEvents, svgPropertiesNoEventsFromUnknown } from '../util/svgPropertiesNoEvents';
-import { XAxisOrientation, XAxisPadding, YAxisOrientation, YAxisPadding } from '../state/cartesianAxisSlice';
+import { AxisId, XAxisOrientation, XAxisPadding, YAxisOrientation, YAxisPadding } from '../state/cartesianAxisSlice';
 import { getCalculatedYAxisWidth } from '../util/YAxisUtils';
 import { RequiresDefaultProps, resolveDefaultProps } from '../util/resolveDefaultProps';
 import { ZIndexable, ZIndexLayer } from '../zIndex/ZIndexLayer';
 import { DefaultZIndexes } from '../zIndex/DefaultZIndexes';
 import { getClassNameFromUnknown } from '../util/getClassNameFromUnknown';
+import { removeRenderedTicks, setRenderedTicks } from '../state/renderedTicksSlice';
+import { useAppDispatch } from '../state/hooks';
 
 /** The orientation of the axis in correspondence to the chart */
 export type Orientation = XAxisOrientation | YAxisOrientation;
@@ -42,6 +45,7 @@ export type TickFormatter = (value: any, index: number) => string;
 export interface CartesianAxisProps extends ZIndexable {
   className?: string;
   axisType?: 'xAxis' | 'yAxis';
+  axisId?: AxisId;
   x?: number;
   y?: number;
   width?: number;
@@ -297,6 +301,41 @@ function TickItem(props: { option: Props['tick']; tickProps: TextProps; value: s
   return tickItem;
 }
 
+function RenderedTicksReporter({
+  ticks,
+  axisType,
+  axisId,
+}: {
+  ticks: ReadonlyArray<TickItemType>;
+  axisType: 'xAxis' | 'yAxis' | undefined;
+  axisId: AxisId | undefined;
+}) {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    if (axisId == null || axisType == null) {
+      return noop;
+    }
+    // Filter out irrelevant internal properties before exposing externally
+    const tickItems = ticks.map(tick => ({
+      value: tick.value,
+      coordinate: tick.coordinate,
+      offset: tick.offset,
+      index: tick.index,
+    }));
+    dispatch(setRenderedTicks({ ticks: tickItems, axisId, axisType }));
+    return () => {
+      dispatch(
+        removeRenderedTicks({
+          axisId,
+          axisType,
+        }),
+      );
+    };
+  }, [dispatch, ticks, axisId, axisType]);
+
+  return null;
+}
+
 type TicksProps = {
   axisType: 'xAxis' | 'yAxis' | undefined;
   events: Omit<PresentationAttributesAdaptChildEvent<any, SVGElement>, 'scale' | 'viewBox'>;
@@ -319,6 +358,7 @@ type TicksProps = {
   width: number;
   x: number;
   y: number;
+  axisId: AxisId | undefined;
 };
 
 const Ticks = forwardRef<SVGGElement, TicksProps>((props: TicksProps, ref) => {
@@ -344,6 +384,7 @@ const Ticks = forwardRef<SVGGElement, TicksProps>((props: TicksProps, ref) => {
     getTicksConfig,
     events,
     axisType,
+    axisId,
   } = props;
   // @ts-expect-error some properties are optional in props but required in getTicks
   const finalTicks = getTicks({ ...getTicksConfig, ticks }, fontSize, letterSpacing);
@@ -425,6 +466,7 @@ const Ticks = forwardRef<SVGGElement, TicksProps>((props: TicksProps, ref) => {
 
   return (
     <g className={`recharts-cartesian-axis-ticks recharts-${axisType}-ticks`}>
+      <RenderedTicksReporter ticks={finalTicks} axisId={axisId} axisType={axisType} />
       {tickLabels.length > 0 && (
         <ZIndexLayer zIndex={DefaultZIndexes.label}>
           <g className={`recharts-cartesian-axis-tick-labels recharts-${axisType}-tick-labels`} ref={ref}>
@@ -440,7 +482,7 @@ const Ticks = forwardRef<SVGGElement, TicksProps>((props: TicksProps, ref) => {
 });
 
 const CartesianAxisComponent = forwardRef<CartesianAxisRef, InternalProps>((props, ref) => {
-  const { axisLine, width, height, className, hide, ticks, axisType, ...rest } = props;
+  const { axisLine, width, height, className, hide, ticks, axisType, axisId, ...rest } = props;
   const [fontSize, setFontSize] = useState('');
   const [letterSpacing, setLetterSpacing] = useState('');
   const tickRefs = useRef<HTMLCollectionOf<Element> | null>(null);
@@ -529,6 +571,7 @@ const CartesianAxisComponent = forwardRef<CartesianAxisRef, InternalProps>((prop
           width={props.width}
           x={props.x}
           y={props.y}
+          axisId={axisId}
         />
         <CartesianLabelContextProvider
           x={props.x}

src/cartesian/XAxis.tsx
@@ -169,6 +169,21 @@ interface XAxisProps<DataPointType = any, DataValueType = any> extends Omit<
    * The margin between tick line and tick.
    */
   tickMargin?: number;
+  /**
+   * Specifies the font size of tick text.
+   * If this prop is not specified, Recharts will read the default tick size from the DOM.
+   *
+   * Font size is used to calculate the width of the ticks, which in turn is used to determine how many ticks are rendered without overlapping.
+   */
+  fontSize?: number | string;
+  /**
+   * Specifies the letter spacing of ticks.
+   * This is used in conjunction with fontSize to calculate the width of the ticks.
+   * If this prop is not specified, Recharts will read the default letter spacing from the DOM.
+   *
+   * Letter spacing is used to calculate the width of the ticks, which in turn is used to determine how many ticks are rendered without overlapping.
+   */
+  letterSpacing?: number | string;
 }
 
 export type Props = Omit<PresentationAttributesAdaptChildEvent<TickItem, SVGTextElement>, 'scale' | 'ref'> & XAxisProps;
@@ -249,6 +264,7 @@ const XAxisImpl = (props: PropsWithDefaults) => {
       viewBox={viewBox}
       ticks={cartesianTickItems}
       axisType={axisType}
+      axisId={xAxisId}
     />
   );
 };

src/cartesian/YAxis.tsx
@@ -184,6 +184,21 @@ interface YAxisProps<DataPointType = any, DataValueType = any> extends Omit<
    * The margin between tick line and tick.
    */
   tickMargin?: number;
+  /**
+   * Specifies the font size of tick text.
+   * If this prop is not specified, Recharts will read the default tick size from the DOM.
+   *
+   * Font size is used to calculate the width of the ticks, which in turn is used to determine how many ticks are rendered without overlapping.
+   */
+  fontSize?: number | string;
+  /**
+   * Specifies the letter spacing of ticks.
+   * This is used in conjunction with fontSize to calculate the width of the ticks.
+   * If this prop is not specified, Recharts will read the default letter spacing from the DOM.
+   *
+   * Letter spacing is used to calculate the width of the ticks, which in turn is used to determine how many ticks are rendered without overlapping.
+   */
+  letterSpacing?: number | string;
 }
 
 export type Props = Omit<PresentationAttributesAdaptChildEvent<TickItem, SVGTextElement>, 'scale' | 'ref'> & YAxisProps;
@@ -309,6 +324,7 @@ function YAxisImpl(props: PropsWithDefaults) {
       viewBox={viewBox}
       ticks={cartesianTickItems}
       axisType={axisType}
+      axisId={yAxisId}
     />
   );
 }

src/hooks.ts
@@ -7,7 +7,7 @@ import {
   selectAxisInverseTickSnapScale,
   selectAxisScale,
   selectAxisWithScale,
-  selectTicksOfAxis,
+  selectRenderedTicksOfAxis,
 } from './state/selectors/axisSelectors';
 import { useAppSelector } from './state/hooks';
 import { useIsPanorama } from './context/PanoramaContext';
@@ -20,7 +20,7 @@ import {
 import { ChartOffset, PlotArea } from './types';
 import { selectChartOffset } from './state/selectors/selectChartOffset';
 import { selectPlotArea } from './state/selectors/selectPlotArea';
-import { CategoricalDomain, Coordinate, NumberDomain, CartesianTickItem } from './util/types';
+import { CategoricalDomain, Coordinate, NumberDomain, TickItem } from './util/types';
 import { ActiveLabel } from './synchronisation/types';
 import { BandPosition } from './util/scale/RechartsScale';
 
@@ -174,8 +174,7 @@ export const useXAxisInverseDataSnapScale = (xAxisId: AxisId = defaultAxisId): I
  * @since 3.8
  */
 export const useXAxisInverseTickSnapScale = (xAxisId: AxisId = defaultAxisId): InverseScaleFunction | undefined => {
-  const isPanorama = useIsPanorama();
-  return useAppSelector(state => selectAxisInverseTickSnapScale(state, 'xAxis', xAxisId, isPanorama));
+  return useAppSelector(state => selectAxisInverseTickSnapScale(state, 'xAxis', xAxisId));
 };
 
 /**
@@ -248,8 +247,7 @@ export const useYAxisInverseDataSnapScale = (yAxisId: AxisId = defaultAxisId): I
  * @since 3.8
  */
 export const useYAxisInverseTickSnapScale = (yAxisId: AxisId = defaultAxisId): InverseScaleFunction | undefined => {
-  const isPanorama = useIsPanorama();
-  return useAppSelector(state => selectAxisInverseTickSnapScale(state, 'yAxis', yAxisId, isPanorama));
+  return useAppSelector(state => selectAxisInverseTickSnapScale(state, 'yAxis', yAxisId));
 };
 
 /**
@@ -262,9 +260,8 @@ export const useYAxisInverseTickSnapScale = (yAxisId: AxisId = defaultAxisId): I
  * @returns An array of ticks, or `undefined` if the axis doesn't exist or hasn't been calculated yet.
  * @since 3.8
  */
-export const useXAxisTicks = (xAxisId: AxisId = defaultAxisId): ReadonlyArray<CartesianTickItem> | undefined => {
-  const isPanorama = useIsPanorama();
-  return useAppSelector(state => selectTicksOfAxis(state, 'xAxis', xAxisId, isPanorama));
+export const useXAxisTicks = (xAxisId: AxisId = defaultAxisId): ReadonlyArray<TickItem> | undefined => {
+  return useAppSelector(state => selectRenderedTicksOfAxis(state, 'xAxis', xAxisId));
 };
 
 /**
@@ -277,9 +274,8 @@ export const useXAxisTicks = (xAxisId: AxisId = defaultAxisId): ReadonlyArray<Ca
  * @returns An array of ticks, or `undefined` if the axis doesn't exist or hasn't been calculated yet.
  * @since 3.8
  */
-export const useYAxisTicks = (yAxisId: AxisId = defaultAxisId): ReadonlyArray<CartesianTickItem> | undefined => {
-  const isPanorama = useIsPanorama();
-  return useAppSelector(state => selectTicksOfAxis(state, 'yAxis', yAxisId, isPanorama));
+export const useYAxisTicks = (yAxisId: AxisId = defaultAxisId): ReadonlyArray<TickItem> | undefined => {
+  return useAppSelector(state => selectRenderedTicksOfAxis(state, 'yAxis', yAxisId));
 };
 
 /**

src/state/renderedTicksSlice.ts
@@ -1,0 +1,59 @@
+/**
+ * @fileOverview this stores actually rendered ticks.
+ *
+ * What we do is that we have the domain -> ticks mapping in the cartesianSlice,
+ * which is fine but the result then goes to CartesianAxis where we use DOM measurement
+ * to decide which ticks to actually render.
+ *
+ * This renderedTickSlice stores those actually rendered ticks so that we can return them from a hook later.
+ */
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { castDraft, WritableDraft } from 'immer';
+import { TickItem } from '../util/types';
+import { AxisId } from './cartesianAxisSlice';
+
+type RenderedTicksAxisState = {
+  [axisId: AxisId]: ReadonlyArray<TickItem>;
+};
+
+type RenderedTicksState = {
+  xAxis: RenderedTicksAxisState;
+  yAxis: RenderedTicksAxisState;
+};
+
+const initialState: RenderedTicksState = {
+  xAxis: {},
+  yAxis: {},
+};
+
+export const renderedTicksSlice = createSlice({
+  name: 'renderedTicks',
+  initialState,
+  reducers: {
+    setRenderedTicks: (
+      state: WritableDraft<RenderedTicksState>,
+      action: PayloadAction<{
+        axisType: 'xAxis' | 'yAxis';
+        axisId: AxisId;
+        ticks: ReadonlyArray<TickItem>;
+      }>,
+    ) => {
+      const { axisType, axisId, ticks } = action.payload;
+      state[axisType][axisId] = castDraft(ticks);
+    },
+    removeRenderedTicks: (
+      state: WritableDraft<RenderedTicksState>,
+      action: PayloadAction<{
+        axisType: 'xAxis' | 'yAxis';
+        axisId: AxisId;
+      }>,
+    ) => {
+      const { axisType, axisId } = action.payload;
+      delete state[axisType][axisId];
+    },
+  },
+});
+
+export const { setRenderedTicks, removeRenderedTicks } = renderedTicksSlice.actions;
+
+export const renderedTicksReducer = renderedTicksSlice.reducer;

src/state/selectors/axisSelectors.ts
@@ -2145,14 +2145,21 @@ export const selectChartDirection: (state: RechartsRootState) => AxisDirection |
   },
 );
 
+export const selectRenderedTicksOfAxis: (
+  state: RechartsRootState,
+  axisType: 'xAxis' | 'yAxis',
+  axisId: AxisId,
+) => ReadonlyArray<TickItem> | undefined = (state, axisType, axisId) => {
+  return state.renderedTicks[axisType]?.[axisId];
+};
+
 export const selectAxisInverseTickSnapScale: (
   state: RechartsRootState,
-  axisType: RenderableAxisType,
+  axisType: 'xAxis' | 'yAxis',
   axisId: AxisId,
-  isPanorama: boolean,
 ) => InverseScaleFunction | undefined = createSelector(
-  [selectTicksOfAxis],
-  (ticks: ReadonlyArray<CartesianTickItem> | undefined) => {
+  [selectRenderedTicksOfAxis],
+  (ticks: ReadonlyArray<TickItem> | undefined) => {
     if (!ticks || ticks.length === 0) {
       return undefined;
     }

src/state/store.ts
@@ -20,6 +20,7 @@ import { errorBarReducer } from './errorBarSlice';
 import { Global } from '../util/Global';
 import { zIndexReducer } from './zIndexSlice';
 import { eventSettingsReducer } from './eventSettingsSlice';
+import { renderedTicksReducer } from './renderedTicksSlice';
 
 export type RechartsRootState = {
   brush: ReturnType<typeof brushReducer>;
@@ -34,6 +35,7 @@ export type RechartsRootState = {
   polarAxis: ReturnType<typeof polarAxisReducer>;
   polarOptions: ReturnType<typeof polarOptionsReducer>;
   referenceElements: ReturnType<typeof referenceElementsReducer>;
+  renderedTicks: ReturnType<typeof renderedTicksReducer>;
   rootProps: ReturnType<typeof rootPropsReducer>;
   tooltip: ReturnType<typeof tooltipReducer>;
   zIndex: ReturnType<typeof zIndexReducer>;
@@ -52,6 +54,7 @@ const rootReducer: Reducer<RechartsRootState> = combineReducers({
   polarAxis: polarAxisReducer,
   polarOptions: polarOptionsReducer,
   referenceElements: referenceElementsReducer,
+  renderedTicks: renderedTicksReducer,
   rootProps: rootPropsReducer,
   tooltip: tooltipReducer,
   zIndex: zIndexReducer,

commit ca5f9ec705b03a41a8c79020259500140e40362b

fix: respect user-provided textAnchor prop on XAxis/YAxis (#7028)
Fixes a regression introduced in #6990 where the `textAnchor` prop on
XAxis/YAxis was being ignored.

The calculated `textAnchor` (from orientation/mirror) was overriding the
user-provided value because it was assigned after spreading `axisProps`.

**Change:** Now uses user-provided `textAnchor` if present, otherwise
falls back to the calculated value.

Fixes #7027

<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->
## Summary by CodeRabbit

* **Improvements**
* Chart tick labels now respect an explicit horizontal text alignment
when provided; otherwise they fall back to the previous automatic
horizontal alignment. Vertical alignment behavior remains unchanged.

* **Tests**
* Added tests that verify tick label horizontal alignment when a
user-specified alignment is present and when defaults are used, and
confirm the expected tick count.
<!-- end of auto-generated comment: release notes by coderabbit.ai -->

---------

Co-authored-by: Jarvis <jarvis@pierreeurope.github>

src/cartesian/CartesianAxis.tsx
@@ -7,7 +7,7 @@ import { SVGProps, useState, useRef, useCallback, forwardRef, useImperativeHandl
 import get from 'es-toolkit/compat/get';
 import { clsx } from 'clsx';
 import { Layer } from '../container/Layer';
-import { Text, Props as TextProps, TextAnchor, TextVerticalAnchor } from '../component/Text';
+import { Text, Props as TextProps, TextAnchor, TextVerticalAnchor, isValidTextAnchor } from '../component/Text';
 import { CartesianLabelContextProvider, ImplicitLabelType, CartesianLabelFromLabelProp } from '../component/Label';
 import { isNumber, noop } from '../util/DataUtils';
 import {
@@ -388,10 +388,13 @@ const Ticks = forwardRef<SVGGElement, TicksProps>((props: TicksProps, ref) => {
   } = props;
   // @ts-expect-error some properties are optional in props but required in getTicks
   const finalTicks = getTicks({ ...getTicksConfig, ticks }, fontSize, letterSpacing);
-  const textAnchor: TextAnchor = getTickTextAnchor(orientation, mirror);
-  const verticalAnchor: TextVerticalAnchor = getTickVerticalAnchor(orientation, mirror);
   const axisProps = svgPropertiesNoEvents(getTicksConfig);
   const customTickProps = svgPropertiesNoEventsFromUnknown(tick);
+  // Use user-provided textAnchor if available, otherwise calculate from orientation/mirror
+  const textAnchor: TextAnchor = isValidTextAnchor(axisProps.textAnchor)
+    ? axisProps.textAnchor
+    : getTickTextAnchor(orientation, mirror);
+  const verticalAnchor: TextVerticalAnchor = getTickVerticalAnchor(orientation, mirror);
   let tickLinePropsObject: SVGProps<SVGLineElement> = {};
   if (typeof tickLine === 'object') {
     tickLinePropsObject = tickLine;

test/cartesian/CartesianAxis.spec.tsx
@@ -356,4 +356,49 @@ describe('<CartesianAxis />', () => {
       expect(element).toHaveClass('recharts-cartesian-axis-tick-value');
     });
   });
+
+  it('Respects user-provided textAnchor prop on horizontal axis', () => {
+    const { container } = render(
+      <Surface width={500} height={500}>
+        <CartesianAxis
+          orientation="bottom"
+          y={100}
+          width={400}
+          height={50}
+          viewBox={{ x: 0, y: 0, width: 500, height: 500 }}
+          ticks={ticks}
+          textAnchor="start"
+          scale={exampleScale}
+        />
+      </Surface>,
+    );
+
+    const tickTexts = container.querySelectorAll('.recharts-cartesian-axis-tick-value');
+    expect(tickTexts).toHaveLength(5);
+    tickTexts.forEach(text => {
+      expect(text).toHaveAttribute('text-anchor', 'start');
+    });
+  });
+
+  it('Uses default textAnchor when not provided', () => {
+    const { container } = render(
+      <Surface width={500} height={500}>
+        <CartesianAxis
+          orientation="bottom"
+          y={100}
+          width={400}
+          height={50}
+          viewBox={{ x: 0, y: 0, width: 500, height: 500 }}
+          ticks={ticks}
+          scale={exampleScale}
+        />
+      </Surface>,
+    );
+
+    const tickTexts = container.querySelectorAll('.recharts-cartesian-axis-tick-value');
+    expect(tickTexts).toHaveLength(5);
+    tickTexts.forEach(text => {
+      expect(text).toHaveAttribute('text-anchor', 'middle');
+    });
+  });
 });